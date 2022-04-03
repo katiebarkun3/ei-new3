785265663945fa507485aa29e6e2dc1afbb84c35
katiebarkun3 <92594853+katiebarkun3@users.noreply.github.com>
2022-04-03 19:29:44 +0300

    Add files via upload

---
+++ settings.h
@@ -0,0 +1,43 @@
+#pragma once
+#include <string>
+
+const int PLAYER_HP = 20;
+
+const float WINDOW_WIDTH = 600.f;
+const float WINDOW_HEIGHT = 900.f;
+const float PLAYER_OFFSET_Y = 10.f;
+const float PLAYER_SPEED_X = 5.5f;
+
+const size_t FPS = 60;
+const size_t FOOD_TYPE_QTY = 4;
+const size_t FOODS_QTY = 3;
+const size_t INEDIBLE_TYPE_QTY = 4;
+const size_t INEDIBLES_QTY = 3;
+const size_t POTION_TYPE_QTY = 1;
+const size_t POTIONS_QTY = 1;
+const size_t DEATH_TYPE_QTY = 1;
+const size_t DEATHS_QTY = 1;
+
+const std::string WINDOW_TITLE = "EDIBLE_INEDIBLE"; 
+const std::string IMAGES_FOLDER = "images/";
+const std::string BACK12_IMAGES_FOLDER = "back12/";
+const std::string BACK23_IMAGES_FOLDER = "back23/";
+const std::string BACK34_IMAGES_FOLDER = "back34/";
+const std::string BACK45_IMAGES_FOLDER = "back45/";
+const std::string BACK56_IMAGES_FOLDER = "back56/";
+const std::string BACK61_IMAGES_FOLDER = "back61/";
+
+const std::string GAME_OVER_FILE_NAME = "game_over.png";
+const std::string RULES_SCREEN = "rulesrus.png";
+const std::string PLAYER_FILE_NAME = "playerfront1.png";
+const std::string BACKGROUND1_FILE_NAME = "back1.jpg";
+const std::string BACKGROUND2_FILE_NAME = "back2.jpg";
+const std::string BACKGROUND3_FILE_NAME = "back3.jpg";
+const std::string BACKGROUND4_FILE_NAME = "back4.jpg";
+const std::string BACKGROUND5_FILE_NAME = "back5.jpg";
+const std::string BACKGROUND6_FILE_NAME = "back6.jpg";
+const std::string SPLASH_FILE_NAME = "splash1.png";
+const std::string HPGIRLS3_FILE_NAME = "3hp.png";
+const std::string PLAYER_RIGHT_FILE_NAME = "playerright1.png";
+const std::string PLAYER_LEFT_FILE_NAME = "playerleft1.png";
+
